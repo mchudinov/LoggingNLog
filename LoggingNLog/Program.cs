44c51a6bff8dfe3c19316da509bc956a98fe1b39
Mikael Chudinov <mikael.chudinov@datametrix.no>
2015-03-02 15:59:05 +0100

    NLog works.

--- LoggingNLog/Program.cs
+++ LoggingNLog/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace LoggingNLog
 {
     class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+            logger.Trace("Sample trace message");
+            logger.Debug("Sample debug message");
+            logger.Info("Sample informational message");
+            logger.Warn("Sample warning message");
+            logger.Error("Sample error message");
+            logger.Fatal("Sample fatal error message");
         }
     }
 }
